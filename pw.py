d39a89245d7ddadf53cd05e3e8c384ee81d16f42
sjung <sjung2393@gmail.com>
2017-08-16 16:34:20 +0800

    Minor pep8 edits

--- pw.py
+++ pw.py
@@ -1,42 +1,36 @@
-# A simple password retriever
-import os
+import argparse
 import json
-import sys
 import pyperclip
+import os
+import sys
 
 
-def addNew():
-
+def add_new():
     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-              "info.txt")) as accFile:
-        ACCOUNT_DATA = json.load(accFile)
+              "info.txt")) as acc_file:
+        ACCOUNT_DATA = json.load(acc_file)
 
-    newAcc = sys.argv[1]
-    newPw = sys.argv[2]
-    confirmNew = input("Add \"{0}\" with \"{1}\" to the dictionary?"
-                       "\ny or n\n".format(newAcc, newPw))
+    new_acc, new_pw = sys.argv[1], sys.argv[2]
 
-    if confirmNew == "y":
-        ACCOUNT_DATA[newAcc] = newPw
-        print("You have added {} to your dictionary".format(newAcc))
+    confirm_new = input('Add "{new_acc}" with "{new_pw}" to the dictionary?'
+                        '\ny or n\n'.format(new_acc=new_acc, new_pw=new_pw))
+    if confirm_new == "y":
+        ACCOUNT_DATA[new_acc] = new_pw
+        print("You have added {} to your dictionary".format(new_acc))
     else:
         print("You have not added a new account")
 
-    accString = json.dumps(ACCOUNT_DATA)
-
+    acc_string = json.dumps(ACCOUNT_DATA, acc_file)
     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-              "info.txt"), "r+") as accFile:
-        accFile.write(accString)
+              "info.txt"), "r+") as acc_file:
+        acc_file.write(acc_string)
 
 
 def retrieve():
-
     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-              "info.txt")) as accFile:
-        ACCOUNT_DATA = json.load(accFile)
-
+              "info.txt")) as acc_file:
+        ACCOUNT_DATA = json.load(acc_file)
     account = sys.argv[1]
-
     if account in ACCOUNT_DATA:
         pyperclip.copy(ACCOUNT_DATA[account])
         print("Password for '{}' copied to clipboard.".format(account))
@@ -45,35 +39,50 @@ def retrieve():
 
 
 def update():
-
     print("An account with this name already exists.")
-
-    accFile = open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                   "info.txt"), "r+")
-
-    ACCOUNT_DATA = json.load(accFile, strict=False)
-    confirmUpdate = input("Update '{0}' with '{1}'?\n"
-                          .format(sys.argv[1], sys.argv[2]))
-    if confirmUpdate == "y":
+    acc_file = open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                    "info.txt"), "r+")
+    ACCOUNT_DATA = json.load(acc_file, strict=False)
+    confirm_update = input('Update "{new_acc}" with "{new_pw}"?\n'
+                           .format(new_acc=sys.argv[1], new_pw=sys.argv[2]))
+    if confirm_update == "y":
         ACCOUNT_DATA.update({str(sys.argv[1]): sys.argv[2]})
+        acc_string = json.dumps(ACCOUNT_DATA)
+        # acc_string.replace('“', '"')
+        acc_file.truncate(0)
+        acc_file.seek(0)
+        acc_file.write(acc_string)
+        acc_file.close()
+        print("{} has been updated.".format(sys.argv[1]))
     else:
         print("Not updated")
 
-    accString = json.dumps(ACCOUNT_DATA)
-    accString.replace('“', '"')
-    accFile.truncate(0)
-    accFile.seek(0)
-    accFile.write(accString)
-    accFile.close()
-    print("{} has been updated.".format(sys.argv[1]))
+
+def delete():
+    acc_file = open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                    "info.txt"), "r+")
+    ACCOUNT_DATA = json.load(acc_file, strict=False)
+    if sys.argv[1] in ACCOUNT_DATA:
+        confirm_delete = input("Delete {}?\n".format(sys.argv[1]))
+        if confirm_delete == "y":
+            del ACCOUNT_DATA[sys.argv[1]]
+            acc_string = json.dumps(ACCOUNT_DATA)
+            acc_file.truncate(0)
+            acc_file.seek(0)
+            acc_file.write(acc_string)
+            acc_file.close()
+            print("{} has been removed from the dictionary.".format
+                  (sys.argv[1]))
+    else:
+        print("Account does not exist. Did not delete.")
+        acc_file.close()
 
 
 def main():
-
-    accFile = open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                   "info.txt"))
-    ACCOUNT_DATA = json.load(accFile, strict=False)
-    accFile.close()
+    acc_file = open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                    "info.txt"))
+    ACCOUNT_DATA = json.load(acc_file, strict=False)
+    acc_file.close()
 
     if len(sys.argv) < 2:
         print('usage: python3 {} account - copy account '
@@ -81,27 +90,21 @@ def main():
               'retrieve'.format(sys.argv[0]))
         sys.exit()
 
-# TO-DO
-# if len==3, and argv2==del, func()--> if argv1 in Dict, del. else "acc not exist"
-    elif len(sys.argv) == 3 and sys.argv[1] in ACCOUNT_DATA and sys.argv[2] == "del":
-        confirmDelete = input("Delete {}?".format(sys.argv[1]))
-        if confirmDelete == "y":
-            del ACCOUNT_DATA[sys.argv[1]]
-            with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                      "info.txt"), "r+") as accFile:
-                accString = json.dumps(ACCOUNT_DATA)
-                accFile.truncate(0)
-                accFile.seek(0)
-                accFile.write(accString)
-        else:
-            sys.exit()
+    elif sys.argv[1] == "ls":
+        print("Usernames:")
+        for key in ACCOUNT_DATA:
+            print("-", key)
+
+    elif len(sys.argv) == 3 and sys.argv[2] == "del":
+        delete()
+        sys.exit()
 
     elif len(sys.argv) == 2:
         retrieve()
         sys.exit()
 
     elif len(sys.argv) == 3 and not sys.argv[1] in ACCOUNT_DATA:
-        addNew()
+        add_new()
         sys.exit()
 
     elif len(sys.argv) == 3 and sys.argv[1] in ACCOUNT_DATA:
@@ -111,16 +114,6 @@ def main():
     elif len(sys.argv) > 3:
         print('Too many arguments passed. Try again.')
 
-    elif sys.argv[1] == "ls":
-        print("Usernames:")
-        for key in ACCOUNT_DATA.keys():
-            print("-", key)
-
 
 if __name__ == "__main__":
     main()
-
-# print(__file__)
-# print(os.path.join(os.path.dirname(__file__), '..'))
-# print(os.path.dirname(os.path.realpath(__file__)))
-# print(os.path.abspath(os.path.dirname(__file__)))
