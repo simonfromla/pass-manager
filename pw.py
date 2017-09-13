fc726c01414023a3357b5f52a5ecfecf9069be88
sjung <sjung2393@gmail.com>
2017-09-13 15:53:05 +0700

    Functions receive args, file manager funcs created.

--- pw.py
+++ pw.py
@@ -4,106 +4,125 @@ import pyperclip
 import os
 import sys
 
+DEFAULT_LOCATION = os.environ.get('INFO_LOCATION') or os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), "info.txt")
 
-def add_new():
+
+def load_manager(fp=None):
+    """logic for loading account dict from file. Uses
+        json module to unserialize data in the file"""
+    if fp is None:
+        fp = DEFAULT_LOCATION
+    with open(fp) as file:
+        acc_dict = json.load(file)
+    return acc_dict
+
+def write_to_file(data, fp=None):
+    """json-serializes data and writes it to filepath."""
+    if fp is None:
+        fp = DEFAULT_LOCATION
+    # maybe write to a temp file first here, then move it instead
+    with open(fp, 'w') as file:
+        file.truncate(0)
+        json.dump(data, file)
+        # file.seek(0)
+
+def add_new(account, new_value, fp=None):
     """Add a new account and pass combination into the dictionary"""
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-              "info.txt")) as acc_file:
-        ACCOUNT_DATA = json.load(acc_file)
+    account_dict = load_manager(fp)
+    # if account in account_dict:
+    #     raise ValueError("Account {} is already a managed account. Use update instead.".format(account))
+    account_dict[account] = new_value
 
-    new_acc, new_pw = sys.argv[1], sys.argv[2]
-
-    confirm_new = input('Add "{new_acc}" with "{new_pw}" to the dictionary?'
-                        '\n(y/n)\n'.format(new_acc=new_acc, new_pw=new_pw))
-    if confirm_new == "y":
-        ACCOUNT_DATA[new_acc] = new_pw
-        with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                  "info.txt"), "r+") as acc_file:
-            json.dump(ACCOUNT_DATA, acc_file)
-        print("You have added '{}' to your dictionary".format(new_acc))
-    else:
-        print("You have not added a new account.")
+    try:
+        write_to_file(account_dict, fp)
+        print("Saved new entry!")
+    except Exception as e:
+        print("Something went wrong: {}".format(e))
 
 
-def retrieve():
+def retrieve(account, fp=None):
     """Retrieve the value for a given account and copy it to the clipboard"""
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-              "info.txt")) as acc_file:
-        ACCOUNT_DATA = json.load(acc_file)
-    account = sys.argv[1]
-    if account in ACCOUNT_DATA:
-        pyperclip.copy(ACCOUNT_DATA[account])
-        print("Password for '{}' copied to clipboard.".format(account))
+    account_dict = load_manager(fp)
+    if account in account_dict:
+        pyperclip.copy(account_dict[account])
+        print("Value for '{}' copied to clipboard.".format(account))
     else:
         print("There is no account named '{}'.".format(account))
 
 
-def update():
+def update(account, new_value, fp=None):
     """Update an existing account with a new value"""
-    print("An account with this name already exists.")
-    confirm_update = input('Update "{new_acc}" with "{new_pw}"?\n(y/n)\n'
-                           .format(new_acc=sys.argv[1], new_pw=sys.argv[2]))
-    if confirm_update == "y":
-        with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                  "info.txt"), "r+") as acc_file:
-            ACCOUNT_DATA = json.load(acc_file)
-            ACCOUNT_DATA.update({str(sys.argv[1]): sys.argv[2]})
-            acc_file.truncate(0)
-            acc_file.seek(0)
-            json.dump(ACCOUNT_DATA, acc_file)
-            print("'{}' has been updated.".format(sys.argv[1]))
-    else:
-        print("Not updated.")
+    account_dict = load_manager(fp)
+    account_dict.update({account:new_value})
+
+    try:
+        write_to_file(account_dict, fp)
+        print("Updated the entry!")
+    except Exception as e:
+        print("Something went wrong: {}".format(e))
 
 
-def delete():
+def delete(account, fp=None):
     """Delete the given account from the dictionary"""
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-              "info.txt"), "r+") as acc_file:
-        ACCOUNT_DATA = json.load(acc_file)
-        if sys.argv[2] in ACCOUNT_DATA:
-            confirm_delete = input("Delete '{}'?\n(y/n)\n".format(sys.argv[2]))
-            if confirm_delete == "y":
-                del ACCOUNT_DATA[sys.argv[2]]
-                acc_file.truncate(0)
-                acc_file.seek(0)
-                json.dump(ACCOUNT_DATA, acc_file)
-                print("'{}' has been removed from the dictionary.".format
-                      (sys.argv[2]))
-        else:
-            print("Account does not exist. Did not delete.")
+    account_dict = load_manager(fp)
+    del account_dict[account]
+    try:
+        write_to_file(account_dict, fp)
+        print("'{}' has been removed from the dictionary.".format(
+            account))
+    except Exception as e:
+        print("Something went wrong: {}".format(e))
 
 
 def main():
-    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
-              "info.txt")) as acc_file:
-        ACCOUNT_DATA = json.load(acc_file)
-
+    account_dict = load_manager()
     num_args = len(sys.argv)
-
     if num_args < 2:
-        print('usage: python3 {} account - copy account '
-              'password\naccount: name of account whose pw to '
+        print('usage: python3 {} account - copy corresponding account '
+              'value\naccount: name of account whose value to '
               'retrieve'.format(__file__))
         sys.exit()
     elif num_args == 2:
+        # List
         if sys.argv[1] == "ls":
             print("Usernames:")
-            for key in ACCOUNT_DATA:
+            for key in account_dict:
                 print("-", key)
         else:
-            retrieve()
+            retrieve(sys.argv[1])
             sys.exit()
     elif num_args == 3:
         if sys.argv[1] == "del":
-            delete()
-            sys.exit()
-        elif sys.argv[1] not in ACCOUNT_DATA:
-            add_new()
-            sys.exit()
-        elif sys.argv[1] in ACCOUNT_DATA:
-            update()
-            sys.exit()
+            # Delete
+            if sys.argv[2] in account_dict:
+                confirm_delete = input("Delete '{}'?\n(y/n)\n".format(sys.argv[2]))
+                if confirm_delete == "y":
+                    delete(sys.argv[2])
+                    sys.exit()
+                else:
+                    print("Did not delete.")
+            else:
+                print("{} does not exist. Did not delete.".format(sys.argv[2]))
+
+        elif sys.argv[1] not in account_dict:
+            # Add new
+            confirm_new = input('Add "{new_acc}" with "{new_val}" to the dictionary?'
+                    '\n(y/n)\n'.format(new_acc=sys.argv[1], new_val=sys.argv[2]))
+            if confirm_new == "y":
+                add_new(sys.argv[1], sys.argv[2])
+                sys.exit()
+
+        elif sys.argv[1] in account_dict:
+            # Update
+            print("An account with this name already exists.")
+            confirm_update = input('Update "{new_acc}" with "{new_val}"?\n'
+                                   '(y/n)\n'.format(new_acc=sys.argv[1], new_val=sys.argv[2]))
+            if confirm_update == "y":
+                update(sys.argv[1], sys.argv[2])
+                sys.exit()
+            else:
+                print("Not updated.")
     else:
         print('Too many arguments passed. Try again.')
 
